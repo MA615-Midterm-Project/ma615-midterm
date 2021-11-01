78df917b0824f25461948472f2d3418fb37affb7
Mi Zhang <mizh@bu.edu>
2021-11-01 14:39:07 -0400

    update visualization function

---
+++ visualization_function.R
@@ -0,0 +1,82 @@
+library(magrittr)
+library(tidyselect)
+library(readxl)
+library(tidyr)
+library(base)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(tidyverse)
+library(maps)
+library(sf)
+library(cowplot)
+library(usmap)
+
+source(file = "strawbData.R", echo = FALSE)
+
+
+# plot1
+
+# yearly value of strawberries in each state
+
+plot1 <-function(para1){
+  dataset <- strawb1 %>% 
+    filter(measurement.s. == para1)
+  ggplot(data=measure_LB) +
+    geom_bar(mapping = aes(x=factor(chemical.type), y=log(value), fill=state), 
+             stat="identity", position= 'dodge', alpha=0.8) +
+    labs(x="year", y="log of measurement in LB")+
+    coord_polar()+
+    facet_wrap(~factor(year)) +
+    theme_bw()
+}
+
+#plot1("MEASURED IN LB")
+
+
+# plot2
+
+#chemical type related value in each state in LB
+
+# yearly value of strawberries in each state
+plot2 <-function(para1, para2, para3){
+  dataset <- strawb1 %>% 
+    filter(measurement.s. == para1) %>%
+    filter(chemical.type %in% para2) %>%
+    filter(state == para3)
+  ggplot(data = dataset)+ 
+    geom_bar(mapping= aes(x = chemical, y= log(value), fill=chemical.type), 
+             stat='identity', alpha= 0.8)+
+    labs(title="") +
+    coord_polar()+
+    theme_bw()
+}
+
+# plot2("MEASURED IN LB","ORGANIC STATUS", "CALIFORNIA")
+
+# map
+
+map <-function(years = "2019",state = "CALIFORNIA", 
+               chemical = "NOP USDA CERTIFIED", measurement = "MEASURED IN LB"){
+  
+  usmap::plot_usmap(data = strawb1[strawb1$year==years & 
+                                     strawb1$state == state & 
+                                     strawb1$chemical == chemical & 
+                                     strawb1$`measurement.s.` == measurement,], 
+                    
+                    values = "value", 
+                    color = "pink",  size = 1,
+                    labels = T, label_color = "grey") +
+    
+    
+    # geom_point(data = strawb1[strawb1$year==years & 
+    #                                 strawb1$state == state & 
+    #                                strawb1$chemical == chemical & 
+    #                                strawb1$`measurement.s.` == measurement,])+
+    
+    ggplot2::scale_fill_continuous(low = "white", high = "red", name = "", label = scales::comma)+ 
+    ggplot2::theme(legend.position = "right")
+}
+
+map()
+
